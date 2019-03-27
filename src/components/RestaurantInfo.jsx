1fb422a45124532e4fe9f6ffe5c828aa829c885f
mahaveer0496 <mahaveersharma0496@yahoo.com>
2019-03-27 23:57:53 +0530

    Add styles to RestaurantInfo Page

--- src/components/RestaurantInfo.jsx
+++ src/components/RestaurantInfo.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {Text, StyleSheet, View} from 'react-native'
+import {Text, StyleSheet, View, Image} from 'react-native'
 
 export default class RestaurantInfo extends Component {
 	static navigationOptions = ({navigation}) => {
@@ -12,17 +12,48 @@ export default class RestaurantInfo extends Component {
 		const {navigation} = this.props
 
 		return (
-			<View>
-				<Text>
-					{' '}
-					{navigation.getParam(
-						'description',
-						'Restaurant Info',
-					)}{' '}
-				</Text>
+			<View style={styles.ctr}>
+				<View style={styles.thumbnail}>
+					<Image
+						source={{
+							uri :
+								'https://picsum.photos/300/300',
+						}}
+						style={styles.image}
+					/>
+					<Text style={styles.description}>
+						{' '}
+						{navigation.getParam(
+							'description',
+							'Restaurant Info',
+						)}{' '}
+					</Text>
+				</View>
 			</View>
 		)
 	}
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+	ctr         : {
+		flex           : 1,
+		justifyContent : 'flex-start',
+		alignItems     : 'center',
+	},
+	thumbnail   : {
+		marginTop    : 20,
+		elevation    : 4,
+		borderRadius : 8,
+		overflow     : 'hidden',
+	},
+	image       : {
+		width  : 300,
+		height : 300,
+	},
+	description : {
+		fontSize        : 18,
+		padding         : 10,
+		backgroundColor : '#0066cc',
+		color           : 'white',
+	},
+})
