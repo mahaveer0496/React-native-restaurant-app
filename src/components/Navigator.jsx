d6151d4bb3b0f5740aa84557139e8ad4c0e74a37
mahaveer0496 <mahaveersharma0496@yahoo.com>
2019-03-27 23:38:30 +0530

    Add default header styling

--- src/components/Navigator.jsx
+++ src/components/Navigator.jsx
@@ -6,10 +6,23 @@ import {
 import Home from './Home'
 import RestaurantInfo from './RestaurantInfo'
 
-const MainNavigator = createStackNavigator({
-	Home           : {screen: Home},
-	RestaurantInfo : {screen: RestaurantInfo},
-})
+const MainNavigator = createStackNavigator(
+	{
+		Home           : {screen: Home},
+		RestaurantInfo : {screen: RestaurantInfo},
+	},
+	{
+		defaultNavigationOptions : {
+			headerStyle      : {
+				backgroundColor : '#0066cc',
+			},
+			headerTintColor  : '#fff',
+			headerTitleStyle : {
+				fontWeight : 'bold',
+			},
+		},
+	},
+)
 
 const Navigator = createAppContainer(MainNavigator)
 
