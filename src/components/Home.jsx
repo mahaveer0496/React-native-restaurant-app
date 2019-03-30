02645f19c49a8d900807bf185b5934e07d2eaa0e
mahaveer0496 <mahaveersharma0496@yahoo.com>
2019-03-31 01:30:42 +0530

    Add Search functionality

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,37 +1,90 @@
 import React, {Component} from 'react'
-import {Text, StyleSheet, View} from 'react-native'
+import {Text, StyleSheet, View, Alert} from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome5'
 
 import RestaurantListContainer from './RestaurantListContainer'
 import Header from './Header'
+import SearchRestaurant from './SearchRestaurant'
+
+import restaurantList from './../helper/restaurantList'
 
 export default class Home extends Component {
-	static navigationOptions = {
-		headerTitle : (
-			<Header
-				leftContent={
-					<Icon name='home' size={30} color='white' />
-				}
-				rightContent={
-					<Icon
-						name='search-plus'
-						size={30}
-						color='white'
-					/>
-				}
-				title='Home'
-			/>
-		),
+	static navigationOptions = ({navigation}) => {
+		const rightIconClick = navigation.getParam(
+			'rightIconClick',
+		)
+		return {
+			headerTitle : (
+				<Header
+					leftContent={
+						<Icon
+							name='home'
+							size={24}
+							color='white'
+						/>
+					}
+					rightContent={
+						<Icon
+							onPress={rightIconClick}
+							name='plus'
+							size={24}
+							color='white'
+							solid
+						/>
+					}
+					title='Home'
+				/>
+			),
+		}
 	}
+	state = {
+		restaurantList : restaurantList,
+		textFromInput  : '',
+	}
+
+	rightIconClick = () => {
+		Alert.alert('woot')
+	}
+
+	componentDidMount = () => {
+		const {navigation} = this.props
+		navigation.setParams({
+			rightIconClick : this.rightIconClick,
+		})
+	}
+
 	_navigate = (screen, params) => {
 		const {navigate} = this.props.navigation
 		navigate(screen, params)
 	}
+	filterRestaurantByName = (restaurants, name) => {
+		if (name)
+			return restaurants.filter(
+				(restaurant) => restaurant.name.search(name) > 0,
+			)
+		return restaurantList
+	}
+
+	handleInputTextChange = (text) => {
+		this.setState({
+			textFromInput  : text,
+			restaurantList : this.filterRestaurantByName(
+				restaurantList,
+				text,
+			),
+		})
+	}
 
 	render() {
+		const {restaurantList, textFromInput} = this.state
 		return (
 			<View style={styles.home}>
+				<SearchRestaurant
+					text={textFromInput}
+					onChangeText={this.handleInputTextChange}
+				/>
 				<RestaurantListContainer
+					restaurantList={restaurantList}
 					navigate={this._navigate}
 				/>
 			</View>
--- src/components/SearchRestaurant.jsx
+++ src/components/SearchRestaurant.jsx
@@ -1,14 +1,27 @@
-import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
+import React, {Component} from 'react'
+import {Text, StyleSheet, View} from 'react-native'
+import {TextInput} from 'react-native-gesture-handler'
 
 export default class SearchRestaurant extends Component {
-  render() {
-    return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
-    )
-  }
+	render() {
+		const {onChangeText, text} = this.props
+		return (
+			<TextInput
+				style={styles.input}
+				onChangeText={onChangeText}
+				value={text}
+				placeholder='Search restaurant by name'
+			/>
+		)
+	}
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+	input : {
+		padding         : 20,
+		elevation       : 3,
+		backgroundColor : 'white',
+		marginBottom    : 10,
+		marginTop       : 5,
+	},
+})
